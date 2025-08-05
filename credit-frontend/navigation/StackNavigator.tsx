30b0a1e28d268c53fbca551264183513e611b4a8
Peter Gillingham <petergillingham80@gmail.com>
2025-08-05 10:53:53 +0100

    simple navigation setup

---
+++ credit-frontend/navigation/StackNavigator.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import DetailsScreen from '../screens/DetailsScreen';
+
+export type RootStackParamList = {
+  Home: React.FC<any>;
+  Details: React.FC<any>;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+const StackNavigator = () => {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name="Details" component={DetailsScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export default StackNavigator;
